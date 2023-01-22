5fed2aad6930735282e0b0aa6c07e2c30612c98e
simon-off <hellosimonoff@gmail.com>
2023-01-22 12:51:59 +0100

    closes #1; Fixed some styling
    Co-authored-by: DanielAndersson31 <DanielAndersson31@users.noreply.github.com>

--- ElectricityCalculator/Classes/ItemData.cs
+++ ElectricityCalculator/Classes/ItemData.cs
@@ -6,10 +6,12 @@ public enum ItemTimeFormat
     Hours
 }
 
-public class ItemData {
-    // Här behöver vi alla properties som watt, timmar osv.
-    public string Title {get; set;} = "";
-    public double Watt {get; set;} = 0;
-    public double UsageTime{get;set;} = 0;
-    public ItemTimeFormat UsageTimeFormat {get; set;} = ItemTimeFormat.Hours;
+public class ItemData
+{
+    public Guid ID { get; } = Guid.NewGuid();
+
+    public string Title { get; set; } = "";
+    public double Wattage { get; set; }
+    public double UsageTime { get; set; }
+    public ItemTimeFormat UsageTimeFormat { get; set; } = ItemTimeFormat.Hours;
 }
--- ElectricityCalculator/Classes/ItemList.cs
+++ ElectricityCalculator/Classes/ItemList.cs
@@ -1,6 +1,12 @@
 namespace ElectricityCalculator.Classes;
 
-public class ItemList 
+public class ItemList
 {
     public List<ItemData> Items = new List<ItemData>();
+
+    public void RemoveItem(Guid ID)
+    {
+        var itemToDelete = Items.Find((item) => item.ID == ID);
+        Items.Remove(itemToDelete);
+    }
 }
