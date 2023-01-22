e2aa03947f242ae22d2939d23cc16a438dc53944
simon-off <hellosimonoff@gmail.com>
2023-01-22 17:12:25 +0100

    closes #2, closes #3; Updated calculations and input regulation
    Co-authored-by: DanielAndersson31 <DanielAndersson31@users.noreply.github.com>

--- ElectricityCalculator/Classes/ItemData.cs
+++ ElectricityCalculator/Classes/ItemData.cs
@@ -8,10 +8,67 @@ public enum ItemTimeFormat
 
 public class ItemData
 {
-    public Guid ID { get; } = Guid.NewGuid();
+    private double _wattage;
+    private double _usageTime;
 
+    public Guid ID { get; } = Guid.NewGuid();
     public string Title { get; set; } = "";
-    public double Wattage { get; set; }
-    public double UsageTime { get; set; }
+    public double Wattage
+    {
+        get { return _wattage; }
+        set
+        {
+            if (value < 0)
+            {
+                _wattage = 0;
+            }
+            else
+            {
+                _wattage = value;
+            }
+        }
+    }
+    public double UsageTime
+    {
+        get { return _usageTime; }
+        set
+        {
+            if (value < 0)
+            {
+                _usageTime = 0;
+            }
+            else if (UsageTimeFormat == ItemTimeFormat.Hours && value > 24)
+            {
+                _usageTime = 24;
+            }
+            else if (UsageTimeFormat == ItemTimeFormat.Minutes && value > 1440)
+            {
+                _usageTime = 1440;
+            }
+            else
+            {
+                _usageTime = value;
+            }
+        }
+    }
     public ItemTimeFormat UsageTimeFormat { get; set; } = ItemTimeFormat.Hours;
+    public double KilowattHours { get; private set; }
+    public double Cost { get; private set; }
+
+    public void UpdateOutput(double price)
+    {
+        Wattage = Wattage;
+        UsageTime = UsageTime;
+
+        if (UsageTimeFormat == ItemTimeFormat.Minutes)
+        {
+            KilowattHours = (_wattage / 1000) * (_usageTime / 60);
+        }
+        else
+        {
+            KilowattHours = (_wattage / 1000) * _usageTime;
+        }
+
+        Cost = KilowattHours * price;
+    }
 }
--- ElectricityCalculator/Classes/ItemList.cs
+++ ElectricityCalculator/Classes/ItemList.cs
@@ -1,8 +1,19 @@
 namespace ElectricityCalculator.Classes;
 
+public enum TotalTimeFormat
+{
+    Day,
+    Month,
+    Year
+}
+
 public class ItemList
 {
     public List<ItemData> Items = new List<ItemData>();
+    public double Price = 250; // ÖRE
+    public double TotalKilowattHours { get; set; }
+    public double TotalCost { get; set; }
+    public TotalTimeFormat TimeFormat { get; set; }
 
     public void RemoveItem(Guid ID)
     {
